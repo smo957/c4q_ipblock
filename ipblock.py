e8b12b1ced307fe4c37eb31a6f9f4aa6ab69b0a1
smo957 <smo957@users.noreply.github.com>
2017-06-02 16:00:29 -0400

    cleaned up ipblock.py and added test

--- ipblock.py
+++ ipblock.py
@@ -8,6 +8,7 @@ Technical Assessment - IP Block Calculator
 """
 
 import argparse
+import unittest
 
 # Instantiate the parser
 parser = argparse.ArgumentParser(description='Check if IP Addesss is in IP block')
@@ -21,8 +22,11 @@ parser.add_argument('address', type=str,
 block = parser.parse_args().block
 address = parser.parse_args().address
 
-print block
-print address
+# Parse and store IP block and address into arrays
+block_array = block.split(".")
+address_array = address.split(".")
+shared_bits = int(block_array[3].split("/")[1])
+block_array[3] = block_array[3].split("/")[0]
 
 ## Check user input
 if "/" not in block:
@@ -31,20 +35,13 @@ if block.count(".") != 3:
     parser.error("IP Block needs exactly three periods.")
 if address.count(".") != 3:
     parser.error("IP Address needs exactly three periods.")
-
-# Parse and store IP block and address into arrays
-block_array = block.split(".")
-address_array = address.split(".")
-shared_bits = int(block_array[3].split("/")[1])
-block_array[3] = block_array[3].split("/")[0]
+if shared_bits > 32:
+    parser.error("IP Address and Block can share up to 32 bits.")
 
 # Function to convert a number in string type to 8-bit binary number
 def toBin(num):
     temp_bin = "{0:b}".format(int(num))
     return '0' * (8-len(temp_bin)) + temp_bin
-#print toBin(12)
-#print toBin(80)
-#print toBin(100)
 
 # Convert IP block and address to binary numbers
 block_binary = ''
@@ -53,12 +50,11 @@ for num in block_array:
     block_binary += toBin(num)
 for num in address_array:
     address_binary += toBin(num)
-#print block_binary
-#print address_binary
-
 
 # Compare IP block and address up to the shared_bits
 if block_binary[:shared_bits] == address_binary[:shared_bits]:
     print "in block"
 else:
     print "not in block"
+
+# Test
